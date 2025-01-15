c7b931b6ddf264de88426a439fa199760f5eca86
VincenzoManto <vincmanto@gmail.com>
2025-01-15 09:15:36 +0100

    feat: refactor flow component to integrate HeatmapFlow and remove unused elements

---
+++ src/features/projects/flow/HeatmapFlow.tsx
@@ -0,0 +1,134 @@
+import { useEffect, useRef } from 'react'
+import {
+  ReactFlow,
+  MiniMap,
+  Controls,
+  Background,
+  Node,
+} from '@xyflow/react'
+import '@xyflow/react/dist/style.css'
+import SimpleHeat from 'simpleheat'
+import { AnimatedSVGEdge } from './AnimatedEdge'
+import AnnotationNode from './AnnotationNode'
+import ButtonEdge from './ButtonEdge'
+import CircleNode from './CircleNode'
+import ResizerNode from './ResizerNode'
+import TextInputNode from './TextInputNode'
+import ToolbarNode from './ToolbarNode'
+import './flow.css'
+
+const nodeTypes = {
+  annotation: AnnotationNode,
+  tools: ToolbarNode,
+  resizer: ResizerNode,
+  circle: CircleNode,
+  textinput: TextInputNode,
+}
+
+const edgeTypes: any = {
+  button: ButtonEdge,
+  animatedSvg: AnimatedSVGEdge,
+}
+
+const nodeClassName = (node: Node) => node.type
+
+export default function HeatmapFlow({
+    onNodesChange,
+    onEdgesChange,
+    nodes,
+    edges,
+    onConnect,
+    }: {
+    onNodesChange: any
+    onEdgesChange: any
+    nodes: Node[]
+    edges: any[]
+    onConnect: any
+}) {
+
+  const canvasRef = useRef<HTMLCanvasElement>(null)
+  const heat = useRef<any>(null)
+  const points = useRef<{ x: number; y: number; v: number }[]>([])
+
+  useEffect(() => {
+    if (canvasRef.current) {
+      const canvas = canvasRef.current
+      canvas.width = canvas.offsetWidth
+      canvas.height = canvas.offsetHeight
+
+      // Inizializza simpleheat
+      heat.current = new SimpleHeat(canvas)
+      setInterval(() => {
+        heat.current.data(
+          points.current
+            .map((e) => {
+              e.v -= 0.025
+              if (e.v < 0) {
+                return null
+              }
+              return [e.x, e.y, e.v]
+            })
+            .filter((e) => e)
+        )
+      }, 100)
+    }
+  }, [])
+
+  const updateHeatmap = (x: number, y: number) => {
+    if (heat.current && canvasRef.current) {
+      heat.current.add([x, y, 0.5])
+      points.current.push({ x, y, v: 0.5 })
+    }
+  }
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      for (const e of document.getElementsByClassName('animated-edge')) {
+        const circle = e
+        if (circle && canvasRef.current) {
+          const { left, top } = canvasRef.current.getBoundingClientRect()
+          const { x, y } = circle.getBoundingClientRect()
+          const adjustedX = x - left
+          const adjustedY = y - top
+
+          updateHeatmap(adjustedX, adjustedY)
+        }
+      }
+      heat.current.draw(0.05)
+    }, 100)
+
+    return () => clearInterval(interval)
+  }, [])
+
+  return (
+    <div style={{ position: 'relative', width: '100%', height: '100%' }}>
+      <canvas
+        ref={canvasRef}
+        style={{
+          position: 'absolute',
+          top: 0,
+          left: 0,
+          width: '100%',
+          height: '100%',
+        }}
+      />
+      <ReactFlow
+        nodes={nodes}
+        edges={edges}
+        onNodesChange={onNodesChange}
+        onEdgesChange={onEdgesChange}
+        onConnect={onConnect}
+        fitView
+        colorMode='dark'
+        attributionPosition='top-right'
+        nodeTypes={nodeTypes}
+        edgeTypes={edgeTypes}
+        style={{ backgroundColor: '#F7F9FB' }}
+      >
+        <MiniMap zoomable pannable nodeClassName={nodeClassName} />
+        <Controls />
+        <Background />
+      </ReactFlow>
+    </div>
+  )
+}
