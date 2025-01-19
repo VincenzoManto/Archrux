9ee63572bfcadb1947937e1b63efaa53d67fc8e0
VincenzoManto <vincmanto@gmail.com>
2025-01-19 18:31:00 +0100

    feat: refactor LineNode and ScatterNode to use ChartBaseNode for improved code reuse and maintainability

---
+++ src/features/projects/flow/nodes/Charts/ChartBaseNode.tsx
@@ -0,0 +1,140 @@
+import { memo, useEffect, useRef, useState } from 'react'
+import { IconGripVertical } from '@tabler/icons-react'
+import {
+  Handle,
+  Node,
+  NodeProps,
+  Position,
+  useHandleConnections,
+  useNodesData,
+  useReactFlow,
+} from '@xyflow/react'
+import * as echarts from 'echarts'
+import {
+  BarNodeProps,
+  DataNodeProps,
+  Dataset,
+  TransformationNodeProps,
+} from '@/lib/publicTypes'
+import { Label } from '@/components/ui/label'
+import { Separator } from '@/components/ui/separator'
+import { ColumnSelector, MultiColumnSelector } from '../ColumnSelector'
+
+function ChartBaseNode({ id, data, type }: NodeProps<Node<BarNodeProps>>) {
+  const connections = useHandleConnections({
+    type: 'target',
+  })
+  const nodesData = useNodesData<Node<DataNodeProps | TransformationNodeProps>>(
+    connections.map((connection) => connection.source)
+  )
+
+  const [xColumn, setXColumn] = useState<string>(data.xColumn || '')
+  const [yColumns, setYColumns] = useState<string[]>(data.yColumns || [])
+  const [subCategoryColumn, setSubCategoryColumn] = useState<string>(
+    data.subCategoryColumn || 'Column'
+  )
+  const chartRef = useRef<HTMLDivElement>(null)
+  const [input, setInput] = useState<Dataset>([])
+  const { updateNodeData } = useReactFlow()
+
+  useEffect(() => {
+    if (!nodesData?.length) return
+    const input = nodesData[0].data?.output
+    if (input) {
+      setInput(input)
+      if (!xColumn && !yColumns.length) {
+        setXColumn(Object.keys(input[0])[0])
+        setYColumns([Object.keys(input[0])[1]])
+      }
+    }
+    if (!input || !xColumn || !yColumns.length) return
+    const chart = echarts.init(chartRef.current!)
+    const xData = input.map((row: any) => row[xColumn] as string)
+
+    let series = []
+    if (subCategoryColumn && subCategoryColumn !== 'Column') {
+      const subCategories = Array.from(
+        new Set(input.map((row: any) => row[subCategoryColumn]))
+      )
+      series = subCategories.flatMap((subCategory) =>
+        yColumns.map((yColumn) => ({
+          name: `${subCategory} - ${yColumn}`,
+          type,
+          stack: subCategory,
+          data: input
+            .filter((row: any) => row[subCategoryColumn] === subCategory)
+            .map((row: any) => row[yColumn] as number),
+        }))
+      )
+    } else {
+      series = yColumns.map((yColumn) => ({
+        name: yColumn,
+        type,
+        data: input.map((row: any) => row[yColumn] as number),
+      }))
+    }
+
+    chart.setOption({
+      color: [
+        '#00d86f',
+        '#ff5b5b',
+        '#ffcc00',
+        '#00a1ff',
+        '#ff00ff',
+        '#00ff00',
+        '#0000ff',
+        '#ff0000',
+        '#00ffff',
+        '#ffff00',
+      ],
+      xAxis: { type: 'category', name: xColumn, data: xData },
+      yAxis: { type: 'value' },
+      series: series,
+    })
+
+    updateNodeData(id, {
+      xColumn,
+      yColumns,
+      subCategoryColumn,
+      output: input,
+    })
+    return () => chart.dispose()
+  }, [nodesData, xColumn, yColumns, subCategoryColumn])
+
+  return (
+    <div>
+      <div className='drag-handle__custom border-b py-2 text-left mb-2'>
+        <IconGripVertical size={12} className='inline' />
+        Bar
+        <Separator className='shadow' />
+      </div>
+      <Label>X-Axis</Label>
+      <ColumnSelector
+        data={{ input: input }}
+        selectedColumn={xColumn}
+        setSelectedColumn={setXColumn}
+      />
+      <Label>Y-Axis</Label>
+      <MultiColumnSelector
+        data={{ input: input }}
+        selectedColumns={yColumns}
+        setSelectedColumns={setYColumns}
+      />
+      <Label>Subcategory</Label>
+      <ColumnSelector
+        data={{ input: input }}
+        additionalColumns={['Column']}
+        acceptEmpty={true}
+        selectedColumn={subCategoryColumn}
+        setSelectedColumn={setSubCategoryColumn}
+      />
+      <div ref={chartRef} style={{ width: 300, height: 300 }}></div>
+      <Handle
+        type='target'
+        className='custom-handle'
+        position={Position.Left}
+      />
+    </div>
+  )
+}
+export default memo(ChartBaseNode)
--- src/features/projects/flow/nodes/Charts/ScatterNode.tsx
+++ src/features/projects/flow/nodes/Charts/ScatterNode.tsx
@@ -1,84 +1,19 @@
-import { memo, useEffect, useRef, useState } from 'react'
-import { IconGripVertical } from '@tabler/icons-react'
+import { memo } from 'react'
 import {
-  Handle,
   Node,
   NodeProps,
-  Position,
-  useHandleConnections,
-  useNodesData,
 } from '@xyflow/react'
-import * as echarts from 'echarts'
-import { Separator } from '@/components/ui/separator'
-import { DataNodeProps, Dataset, TransformationNodeProps, VisualizationNodeProps } from '@/lib/publicTypes'
-import { ColumnSelector } from '../ColumnSelector'
-import { Label } from '@/components/ui/label'
+import {
+  BarNodeProps,
+} from '@/lib/publicTypes'
+import ChartBaseNode from './ChartBaseNode'
 
-function ScatterNode(props: NodeProps<Node<VisualizationNodeProps>>) {
-  const connections = useHandleConnections({
-    type: 'target',
-  })
-  const nodesData = useNodesData<Node<DataNodeProps | TransformationNodeProps>>(
-    connections.map((connection) => connection.source)
-  )
-  // const textNodes = nodesData.filter(isTextNode);
-
-  const [xColumn, setXColumn] = useState<string>('')
-  const [yColumn, setYColumn] = useState<string>('')
-  const chartRef = useRef<HTMLDivElement>(null)
-  const [input, setInput] = useState<Dataset>([])
-
-  useEffect(() => {
-    if (!nodesData?.length) return
-    const input = nodesData[0].data?.output;
-    if (input) {
-      setInput(input)
-      if (!xColumn && !yColumn) {
-        setXColumn(Object.keys(input[0])[0])
-        setYColumn(Object.keys(input[0])[1])
-      }
-    }
-    if (!input || !xColumn || !yColumn) return
-    const chart = echarts.init(chartRef.current!)
-    const xData = input.map((row: any) => row[xColumn] as number)
-    const yData = input.map((row: any) => row[yColumn] as number)
-
-    chart.setOption({
-      color: ['#00d86f'],
-      xAxis: { type: 'value', name: xColumn },
-      yAxis: { type: 'value', name: yColumn },
-      series: [{ data: xData.map((x: number, i: number) => [x, yData[i]]), type: 'scatter' }],
-    })
-
-    return () => chart.dispose()
-  }, [nodesData, xColumn, yColumn])
-
+function ScatterNode(props: NodeProps<Node<BarNodeProps>>) {
+  
   return (
-    <div>
-      <div className='drag-handle__custom border-b py-2 text-left mb-2'>
-        <IconGripVertical size={12} className='inline' />
-        Scatter
-        <Separator className='shadow' />
-      </div>
-      <Label>X-Axis</Label>
-      <ColumnSelector
-        data={{ input: input }}
-        selectedColumn={xColumn}
-        setSelectedColumn={setXColumn}
-      />
-      <Label>Y-Axis</Label>
-      <ColumnSelector
-        data={{ input: input }}
-        selectedColumn={yColumn}
-        setSelectedColumn={setYColumn}
-      />
-      <div ref={chartRef} style={{ width: 300, height: 300 }}></div>
-      <Handle
-        type='target'
-        className='custom-handle'
-        position={Position.Left}
-      />
-    </div>
+    <ChartBaseNode 
+      {...props}
+    type='scatter' />
   )
 }
 export default memo(ScatterNode)
